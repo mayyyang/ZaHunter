6398f306ae772c094e2c64db6091981e397839fd
May Yang <mayyang@May-Yangs-MacBook-Pro.local>
2014-11-05 15:00:48 -0800

    Initial Commit

---
+++ ZaHunterChallenge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZaHunterChallenge
+//
+//  Created by May Yang on 11/5/14.
+//  Copyright (c) 2014 May Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
